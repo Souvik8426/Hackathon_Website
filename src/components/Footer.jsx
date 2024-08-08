a19709a17483d6e9bd11d62290f91f8f241dad50
Souvik Ruhidas <102573237+Souvik8426@users.noreply.github.com>
2024-08-08 17:42:45 +0530

    Minor changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
 
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Hackathon
-            </span>
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Hackathon
+          </span>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
@@ -32,7 +32,13 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="relative flex justify-center py-5">
+          <div
+            className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"
+          ></div>
+
+          <span className="relative text-white z-10 px-6">Made With Love</span>
+        </span>
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           &copy; 2024{' '}
           <a href="https://flowbite.com/" className="hover:underline">
