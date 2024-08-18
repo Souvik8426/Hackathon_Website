ed39c51c04deb976478385c87e25ea4c4ade543c
Souvik Ruhidas <102573237+Souvik8426@users.noreply.github.com>
2024-08-18 13:36:29 +0530

    Fixed Timeline.jsx

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -29,7 +29,7 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-blue-500 bg-blue-200 shadow-lg"></div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
                     </div>
 
@@ -40,7 +40,7 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-blue-500 bg-blue-200 shadow-lg"></div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
 
                         <div className=" col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md border-2 border-[#A555EC] shadow-[#509f9b54]">
@@ -74,25 +74,44 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-start justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-blue-500 bg-blue-200 shadow-lg"></div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
                     </div>
 
                     {/* Speaker Session */}
 
-                    <div className="flex flex-row-reverse md:contents">
-                        <div className=" col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md border-2 border-[#A555EC] shadow-[#509f9b54]">
-                            {" "}
-                            <h3 className="font-semibold text-lg mb-1 ">Inaguration </h3>
-                            <h5 className=" text-sm text-slate-600">
-                                10:00 am - 11:30 am
-                            </h5>
+                    <div className="flex md:contents">
+                        <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                            <div className="h-full w-6 flex items-center justify-center">
+                                <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
+                            </div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
+                        </div>
+
+                        <div className=" col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md border-2 border-[#A555EC] shadow-[#509f9b54]">
+                            <h3 className="font-semibold text-lg mb-1">March 25 - 30</h3>
+                            <h5 className=" text-sm text-slate-600">09:00 am</h5>
                             <br />
                             <p className="leading-tight text-justify">
-                                Inaguration & Speakers' Session .Join us for the grand
-                                inauguration ceremony followed by enlightening speaker
-                                sessions. Gain insights, inspiration, and motivation from
-                                industry experts and thought leaders.
+                                Approvals will be rolled out between March 25 and March 30.
+                                Keep an eye on your inbox for confirmation 👀!
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* hackathon starts */}
+
+                    <div className="flex flex-row-reverse md:contents">
+                        <div className=" col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md border-2 border-[#A555EC] shadow-[#509f9b54]">
+                            <h3 className="font-semibold text-lg mb-1">
+                                April 06 : Day 01
+                            </h3>
+                            <h5 className=" text-sm text-slate-600">09:00 am</h5>
+                            <br />
+                            <p className="leading-tight text-justify">
+                                Kickstart your day by checking in for the most anticipated
+                                hackathon event of the year. Meet fellow participants and
+                                get ready for an exhilarating experience .
                             </p>
                         </div>
 
@@ -100,30 +119,7 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-start justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-2 border-blue-500 bg-black shadow-lg"></div>
-                        </div>
-                    </div>
-
-                    {/* hackathon starts */}
-
-                    <div className="flex md:contents">
-                        <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-                            <div className="h-full w-6 flex items-center justify-center">
-                                <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
-                            </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-2 border-blue-500 bg-black shadow-lg"></div>
-                        </div>
-
-                        <div className=" col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md border-2 border-[#A555EC] shadow-[#509f9b54]">
-                            <h3 className="font-semibold text-lg mb-1 ">
-                                Hackathon Begins
-                            </h3>
-                            <h5 className=" text-sm text-slate-600">12:00 pm</h5>
-                            <br />
-                            <p className="leading-tight text-justify">
-                                Hacking begin! Dive into your projects, collaborate with
-                                your team, and start turning your ideas into reality.
-                            </p>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
                     </div>
 
@@ -134,7 +130,7 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-2 border-blue-500 bg-black shadow-lg"></div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
 
                         <div className=" col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md border-2 border-[#A555EC] shadow-[#509f9b54]">
@@ -169,7 +165,7 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-2 border-blue-500 bg-black shadow-lg"></div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
                     </div>
 
@@ -180,7 +176,7 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-2 border-blue-500 bg-black shadow-lg"></div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
 
                         <div className=" col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md border-2 border-[#A555EC] shadow-[#509f9b54]">
@@ -216,7 +212,7 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-blue-500 bg-blue-200 shadow-lg"></div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
                     </div>
 
@@ -227,7 +223,7 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-2 border-blue-500 bg-black shadow-lg"></div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
 
                         <div className=" col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md border-2 border-[#A555EC] shadow-[#509f9b54]">
@@ -270,7 +266,7 @@ const Timeline = () => {
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-blue-200 pointer-events-none"></div>
                             </div>
-                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-blue-500 bg-blue-200 shadow-lg"></div>
+                            <div className="w-6 h-6 absolute top-7 -mt-3 rounded-full border-4 border-purple-500 bg-purple-300 shadow-lg"></div>
                         </div>
                     </div>
                 </div>
