90a0b215bd54bf7ad9ec1ddc7b0ed54e8c3bc037
Souvik Ruhidas <102573237+Souvik8426@users.noreply.github.com>
2024-09-02 22:15:25 +0530

    Disabled Header button (register button)

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,6 @@ import Guvi from "./assets/images.png";
 import "./App.css";
 
 const App = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://apply.devfolio.co/v2/sdk.js';
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <div className="bg-transparent">
       <Navbar
